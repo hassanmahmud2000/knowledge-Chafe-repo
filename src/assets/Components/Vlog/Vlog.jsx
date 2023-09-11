2261cb641155eaca819f84ec46c693d566b29057
Mahmudul Hassan <hassanmahmud5556@gmail.com>
2023-09-12 00:08:56 +0600

    Cover User

--- src/assets/Components/Vlog/Vlog.jsx
+++ src/assets/Components/Vlog/Vlog.jsx
@@ -1,9 +1,24 @@
 import PropTypes from 'prop-types';
 
 const Vlog = ({ blog }) => {
+    const  {vlog_title,cover,author_img,author_name,reading_time} = blog
     console.log(blog);
     return (
         <div>
+            <img src= {cover} alt="" />
+            <div className='flex justify-between items-center my-6'>
+                <div className='flex'>
+                    <img className='rounded-full w-[60px]' src= {author_img} alt="" />
+                    <div className='ml-6 '>
+                         <h2 className='text-2xl'>{author_name}</h2>
+                         <p>14 July 2023</p>
+                    </div>
+                </div>
+                <div>
+                    <h3><span> {reading_time}</span> min read</h3>
+                </div>
+            </div>
+            <h2 className='font-bold text-5xl'>{vlog_title}</h2>
             
         </div>
     );
--- src/assets/Components/Vlogs/Vlogs.jsx
+++ src/assets/Components/Vlogs/Vlogs.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Vlog from "../Vlog/Vlog";
 
 const Vlogs = () => {
     const [vlogs,setVlogs] = useState ([])
@@ -11,6 +12,9 @@ const Vlogs = () => {
     return (
         <div className="md:w-2/3">
            <h1 className="text-3xl">Vlogs : {vlogs.length }</h1>
+            {
+                vlogs.map (vlog =>  <Vlog key={vlog.id} blog={vlog}></Vlog>)
+            }
         </div>
     );
 };
